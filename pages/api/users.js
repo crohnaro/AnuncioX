5be6c0141562c146b61f7dacb8cb3f2aeb31c248
crohnaro <viniciosalfa@hotmail.com>
2023-04-20 09:43:14 -0300

    creating the back-end (the api and the db using mongoose)

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,7 @@
 import dbConnect from "../../src/utils/dbConnect"
+import UsersModel from "../../src/models/users"
+
+import { crypto } from "../../src/utils/password"
 
 const users = async (req, res) => {
   const { method } = req
@@ -8,7 +11,30 @@ const users = async (req, res) => {
       await dbConnect()
       res.status(200).json({success: true})
       break
+    
+    case 'POST': 
+
+    const {
+      name, 
+      email, 
+      password,
+    } = req.body
+
+    await dbConnect()
+
+    const passwordCrypto = await crypto(password)
+
+    const user = new UsersModel({
+      name,
+      email,
+      password : passwordCrypto,
+    })
+
+    user.save()
+
+    res.status(201).json({success: true})
   }
+  
 }
 
 export default users
