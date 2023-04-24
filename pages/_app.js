82e6d8d5d15d4711c2049be12a45f66919d235a2
crohnaro <crohnarodev@gmail.com>
2023-04-24 11:43:28 -0300

    finishing the login system using next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { Provider } from "next-auth/client"
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -19,12 +20,14 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <ToastyProvider>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ToastyProvider>
-      </ThemeProvider>
+      <Provider session={pageProps.session}>
+        <ThemeProvider theme={theme}>
+          <ToastyProvider>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ToastyProvider>
+        </ThemeProvider>
+      </Provider>
     </CacheProvider>
   );
 }
