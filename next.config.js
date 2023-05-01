3adcb4a97ab6d157f42204024fc3c352f467464f
crohnaro <crohnarodev@gmail.com>
2023-05-01 11:17:38 -0300

    adding deploy configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: 'mongodb+srv://vinicioscararine95:JyxZp55oaVUkrOXp@cluster0.3pfo9yd.mongodb.net/test'
+  }
 }
 
 module.exports = nextConfig
