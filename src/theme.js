e1fadd930a133f75e1e3c02af78458ee07e362a1
crohnaro <vinicioscararine95@gmail.com>
2023-08-21 12:22:20 -0300

    refactor: :lipstick: improving the UI from the index page

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#000000",
+      main: "#101014",
     },
     secondary: {
       main: "#ffffff",
