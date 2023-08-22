f553812f8fbf81b5a416d14b719e1e34f56feec0
crohnaro <crohnarodev@gmail.com>
2023-08-22 09:57:52 -0300

    refactor: :art: improve the imports and adding the components to toggleTheme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,9 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { AccountCircle, Brightness4, Brightness7 } from "@mui/icons-material";
 
 import { signOut, useSession } from "next-auth/react";
-
-import styles from "../styles/Header.module.css";
+import { useState, useContext } from "react";
 
 import {
   Avatar,
@@ -17,53 +12,88 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  useTheme,
+  Button,
+  Typography,
+  Toolbar,
+  Box,
+  AppBar,
 } from "@mui/material";
-import Link from "next/link"
-import { useState } from "react";
+
+import Link from "next/link";
 import Image from "next/image";
-import logo from '../../public/logo-black.png'
+
+import logo from "../../public/logo-black.png";
+
+import styles from "../styles/Header.module.css";
+import { ColorModeContext } from "../../src/contexts/ColorModeContext";
+import { lightBlue } from "@mui/material/colors";
 
 export default function ButtonAppBar() {
   const [anchorUserMenu, setAnchorUserMenu] = useState(false);
-  const { data : session, status } = useSession()
+  const { data: session, status } = useSession();
 
   const openUserMenu = Boolean(anchorUserMenu);
+
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" elevation={3}>
         <Container maxWidth="lg">
           <Toolbar>
-            
-              <Typography sx={{ flexGrow: 1 }} variant="h6" component="div" >
-                <Link style={{textDecoration: 'none', color: 'white',display: 'flex', alignItems: 'center', padding: '10px'}} href={'/'} >
-                  <Image alt="anunX Logo" width={64} priority src={logo}/>
-                </Link>  
-              </Typography>
-            
-            
-              <Button href={session ? '/user/publish' : '/auth/signin'} color="secondary" variant="contained" endIcon={<AddShoppingCartIcon />}>
-                Anunciar e Vender
-              </Button>
-            
-            {
-              session
-                ? (
-                  <IconButton
-                    color="secondary"
-                    onClick={(e) => setAnchorUserMenu(e.currentTarget)}
-                    sx={{ marginLeft: '18px' }}
-                  >
-                    {
-                      session.user.image 
-                      ? <Avatar className={styles.avatar} src={session.user.image} /> 
-                      : <Avatar>{session.user.name[0].toUpperCase()}</Avatar>
-                    }
-                  </IconButton>
-                )
-                : null
-            }
+            <Typography sx={{ flexGrow: 1 }} variant="h6" component="div">
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  display: "flex",
+                  alignItems: "center",
+                  padding: "10px",
+                }}
+                href={"/"}
+              >
+                <Image alt="anunX Logo" width={64} priority src={logo} />
+              </Link>
+            </Typography>
 
+            <Button
+              href={session ? "/user/publish" : "/auth/signin"}
+              color="secondary"
+              variant="contained"
+              endIcon={<AddShoppingCartIcon />}
+            >
+              Anunciar e Vender
+            </Button>
+
+            <Box>
+              <IconButton
+                sx={{ ml: 1 }}
+                onClick={colorMode.toggleColorMode}
+                color="inherit"
+              >
+                {theme.palette.mode === "dark" ? (
+                  <Brightness7 />
+                ) : (
+                  <Brightness4 />
+                )}
+              </IconButton>
+            </Box>
+
+            {session ? (
+              <IconButton
+                color="secondary"
+                onClick={(e) => setAnchorUserMenu(e.currentTarget)}
+                sx={{ marginLeft: "18px" }}
+              >
+                {session.user.image ? (
+                  <Avatar className={styles.avatar} src={session.user.image} />
+                ) : (
+                  <Avatar>{session.user.name[0].toUpperCase()}</Avatar>
+                )}
+              </IconButton>
+            ) : null}
 
             <Menu
               anchorEl={anchorUserMenu}
@@ -71,25 +101,39 @@ export default function ButtonAppBar() {
               onClose={() => setAnchorUserMenu(null)}
             >
               <MenuItem>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: '#000',
-                }} href="/user/dashboard" passHref>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                  }}
+                  href="/user/dashboard"
+                  passHref
+                >
                   Menus anúncios
                 </Link>
               </MenuItem>
               <MenuItem>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: '#000',
-                }} href="/user/publish" passHref>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                  }}
+                  href="/user/publish"
+                  passHref
+                >
                   Publicar novo anúncio
                 </Link>
               </MenuItem>
               <Divider />
-              <MenuItem onClick={() => signOut({
-                callbackUrl: '/'
-              })}>Sair</MenuItem>
+              <MenuItem
+                onClick={() =>
+                  signOut({
+                    callbackUrl: "/",
+                  })
+                }
+              >
+                Sair
+              </MenuItem>
             </Menu>
           </Toolbar>
         </Container>
