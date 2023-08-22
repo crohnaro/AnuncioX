e370ea5f4a63ae230389ca7785bc06a4fb58587b
crohnaro <crohnarodev@gmail.com>
2023-08-22 20:44:49 -0300

    style: :lipstick: improving ui from index page

--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,14 @@ const Home = ({ products }) => {
   return (
     <TemplateDefault>
       <Container maxWidth="md">
-        <Typography component="h1" variant="h3" align="center" color="primary" marginTop={20}>
+        <Typography component="h1" variant="h3" align="center" color="inherit" marginTop={20}>
           O que deseja encontrar?
         </Typography>
         <SearchInput />
       </Container>
 
       <Container maxWidth="lg" className={styles.cardGrid}>
-        <Typography component="h1" variant="h4" align="center" color="primary">
+        <Typography component="h1" variant="h4" align="center" color="inherit">
           Destaques
         </Typography>
         <br />
