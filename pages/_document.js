a7e0defda8c46597332e23cf9932d1b7d36acb7d
crohnaro <crohnarodev@gmail.com>
2023-08-22 09:56:20 -0300

    refactor: :fire: removing metatag for theme

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import theme from '../src/theme';
 
 
 export default function MyDocument() {
@@ -10,7 +9,7 @@ export default function MyDocument() {
     <Html lang="pt-BR">
       <Head>
         {/* PWA primary color */}
-        <meta name="theme-color" content={theme.palette.primary.main} />
+        
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
       </Head>
