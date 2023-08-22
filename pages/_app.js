6889e1ae7b4861985f39bea11d7d58466b95fc0e
crohnaro <crohnarodev@gmail.com>
2023-08-22 09:55:55 -0300

    feat: :sparkles: adding ColorContextMode to the global scope

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,29 @@
-import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { ToastyProvider } from '../src/contexts/Toasty'
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../src/theme';
-import { SessionProvider } from "next-auth/react"
-import CheckAuth from '../src/components/CheckAuth';
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { ToastyProvider } from "../src/contexts/Toasty";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../src/themes";
+import { SessionProvider } from "next-auth/react";
+import CheckAuth from "../src/components/CheckAuth";
 
-
+import { ColorModeProvider } from "@/src/contexts/ColorModeContext";
 
 export default function MyApp({
-  Component, 
-  pageProps: {session, ...pageProps},
+  Component,
+  pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <ThemeProvider theme={theme}>
+      <ColorModeProvider>
         <ToastyProvider>
           <CssBaseline />
-          {
-            Component.requireAuth
-            ? <CheckAuth Component={Component} pageProps={pageProps} />
-            : <Component {...pageProps} />
-          }  
+          {Component.requireAuth ? (
+            <CheckAuth Component={Component} pageProps={pageProps} />
+          ) : (
+            <Component {...pageProps} />
+          )}
         </ToastyProvider>
-      </ThemeProvider>
+      </ColorModeProvider>
     </SessionProvider>
-);
+  );
 }
-  
-
-  
-
-
